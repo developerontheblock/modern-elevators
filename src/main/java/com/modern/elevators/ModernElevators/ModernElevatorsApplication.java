0a5e2cf32e0eb9d72ef8c9d53c3ba57ca4f85475
Alex Draev <a.draev@codecoda.com>
2021-11-04 15:53:34 +0200

    basic structure of the Spring Boot project and .gitignore file added

---
+++ src/main/java/com/modern/elevators/ModernElevators/ModernElevatorsApplication.java
@@ -0,0 +1,13 @@
+package com.modern.elevators.ModernElevators;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModernElevatorsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModernElevatorsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/modern/elevators/ModernElevators/ModernElevatorsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.modern.elevators.ModernElevators;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModernElevatorsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
